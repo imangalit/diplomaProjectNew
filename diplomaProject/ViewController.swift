d07c63c6ddffae02f7a194ce59eea0f2d94d476c
imangalit <temirkhan.imangail@gmail.com>
2023-03-09 19:28:11 +0600

    Initial Commit

---
+++ diplomaProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  diplomaProject
+//
+//  Created by Имангали on 3/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
